9994d3be69c29f32eae3614082999d7fd762d286
preety4697 <147104856+preety4697@users.noreply.github.com>
2024-01-23 19:29:58 +0000

    Homework

---
+++ src/main/java/com/gorest/constants/Path.java
@@ -0,0 +1,12 @@
+package com.gorest.constants;
+
+public class Path {
+    /*
+     * This is base path for user api
+     */
+    public static final String USERS = "/users";
+
+    // This path for post API
+    public static final String POSTS = "/posts";
+
+}
